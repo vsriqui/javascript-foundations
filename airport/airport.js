f7221989ae36689e1475fb20546825f6e11b092d
Victor Sriqui <victorsriqui@gmail.com>
2023-04-03 05:57:50 -0600

    Add part of airport.js

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,40 @@
 
+function createAirport(n, a, g) {
+  var airport = {
+    name: n,
+    airlines: a,
+    availableGates: g,
+    
+
+
+  }
+
+
+  return airport
+}
+
+function welcomeGuests(air) {
+  return `Welcome to ${air.name}!`
+  
+}
+
+function landPlanes(airpor, landing) {
+  if (airpor.availableGates > landing){
+  airpor.availableGates -= landing
+  return `Success! Current availability is ${airpor.availableGates}.`
+  }
+  else if (airpor.availableGates <= landing){
+    landing -= airpor.availableGates
+    overflow = landing
+    airpor.availableGates = 0
+    return `Oh no! Not enough gates available. Current overflow is ${overflow}.`
+  }
+}
 
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
   // checkAirlineLocations
 };
