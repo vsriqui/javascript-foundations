15288be8d77924cf4f7f6e9c3050fad35b916ba7
Victor Sriqui <victorsriqui@gmail.com>
2023-04-04 00:40:08 -0600

    Add birthdays solves

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,35 @@
+module.exports = {  
+createBirthday,
+celebrateBirthday,
+countBirthdays
+};
 
+function createBirthday(n,m,d) {
+    birthday = {
+        name: n,
+        month: m,
+        day: d,
+    }
+    return birthday
+}
 
-module.exports = {  };
+function celebrateBirthday(n) {
+    a = n.name;
+    b = n.month;
+    c = n.day;
+
+    x = `Today is ${b}/${c}! Happy birthday, ${a}!`
+    return x
+}
+
+function countBirthdays(bday, m) {
+    a = 0;
+    for (i=0; i<bday.length; i++){
+       if (bday[i].month === m) {
+        console.log(x)
+        a += i 
+       }
+
+    }
+    return a
+}
