b1289bbe33e5ac717a134cc53d263ee98c826770
Victor Sriqui <victorsriqui@gmail.com>
2023-04-14 10:59:19 -0600

    Add vending

--- dollar-store-vending-machine/index.js
+++ dollar-store-vending-machine/index.js
@@ -1,2 +1,44 @@
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange
+}
 
-module.exports = {}
+function createItemStock (name, quantity, price){
+    var vending = {
+        name: name || "unknown",
+        quantity: quantity || 0,
+        price: price || 1,
+    }
+    // if (!vending.name){
+    //     vending.name = "unknown"
+    // }
+    // if (!vending.quantity){
+    //     vending.quantity = 0
+    // }
+    // if (!vending.price) {
+    //     vending.price = 1
+    // }
+
+    return vending
+}
+
+function makePurchase (item, money) {
+    console.log(item)
+    if (item.price > money) {
+        return `Sorry, you need at least $${item.price} to make that purchase`
+    }
+    if (item.quantity < 1) {
+        return 'Sorry, there are none left'
+    } else {
+        return `Here are your ${item.name}`
+    }
+
+}
+function collectChange (loose) {
+    var combined = 0;
+    for (i=0; i<loose.length; i++) {
+    combined += loose[i]
+    }
+    return combined
+}
